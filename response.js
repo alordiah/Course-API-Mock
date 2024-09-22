300b934c89f597dede8caced6e9a04a9fe597f76
SingleThreaded 4-Eyes <ademoladavid111@gmail.com>
2024-09-22 18:39:46 +0100

    Add files via upload

---
+++ response.js
@@ -0,0 +1,414 @@
+ const courses = { 
+    courses: [
+    {
+      "id": 1,
+      "title": "The Complete Advanced 6-Week U/UX Design Bootcamp",
+      "description": "Learn how to design, prototype, and conduct user research in this comprehensive UX/UI course.",
+      "image_url": "https://image.url/ux-design-bootcamp.jpg",
+      "author": "Dr. Marley Bator",
+      "students": 1936922,
+      "total_lectures": 490,
+      "total_hours": "65h 33m",
+      "price": 549.00,
+      "discounted_price": 85.00,
+      "discount_percentage": 85,
+      "course_contents": [
+        {
+          "section_title": "Week 1 - Beginner - Introduction to UX designing",
+          "contents": [
+            "Read before you start (02:53)",
+            "Introduction to Figma essentials training course (02:45)",
+            "What is the difference between UI & UX in Figma (05:22)",
+            "What we are making in this Figma course (09:18)",
+            "Class project 02 - Create your own brief (1 Question)"
+          ]
+        }
+      ],
+      "articles": 86,
+      "downloadable_resources": 49,
+      "on_demand_video": "65h",
+      "coding_exercises": 8,
+      "certificate_of_completion": true
+    },
+    {
+      "id": 2,
+      "title": "The Complete JavaScript Course",
+      "description": "Master JavaScript with the most complete course on the market! Go from beginner to advanced by building real-world applications.",
+      "image_url": "https://image.url/javascript-course.jpg",
+      "author": "Jonas Schmedtmann",
+      "students": 2050000,
+      "total_lectures": 423,
+      "total_hours": "69h 35m",
+      "price": 249.00,
+      "discounted_price": 17.99,
+      "discount_percentage": 93,
+      "course_contents": [
+        {
+          "section_title": "Module 1 - Introduction to JavaScript",
+          "contents": [
+            "JavaScript fundamentals (12:15)",
+            "Data types and variables (08:45)"
+          ]
+        }
+      ],
+      "articles": 80,
+      "downloadable_resources": 30,
+      "on_demand_video": "69h",
+      "coding_exercises": 15,
+      "certificate_of_completion": true
+    },
+    {
+      "id": 3,
+      "title": "Python for Data Science and Machine Learning Bootcamp",
+      "description": "A comprehensive course to learn Python programming and data science with real-world projects.",
+      "image_url": "https://image.url/python-data-science.jpg",
+      "author": "Jose Portilla",
+      "students": 1532000,
+      "total_lectures": 320,
+      "total_hours": "42h 12m",
+      "price": 199.00,
+      "discounted_price": 12.99,
+      "discount_percentage": 94,
+      "course_contents": [
+        {
+          "section_title": "Section 1 - Python Basics",
+          "contents": [
+            "Installing Python (06:00)",
+            "Python basics and syntax (09:15)"
+          ]
+        }
+      ],
+      "articles": 50,
+      "downloadable_resources": 25,
+      "on_demand_video": "42h",
+      "coding_exercises": 12,
+      "certificate_of_completion": true
+    },
+    {
+      "id": 4,
+      "title": "React - The Complete Guide (incl Hooks, React Router, Redux)",
+      "description": "Master React.js and build dynamic, high-performance web applications from scratch.",
+      "image_url": "https://image.url/react-course.jpg",
+      "author": "Maximilian Schwarzmüller",
+      "students": 1254000,
+      "total_lectures": 289,
+      "total_hours": "40h 45m",
+      "price": 199.99,
+      "discounted_price": 15.99,
+      "discount_percentage": 92,
+      "course_contents": [
+        {
+          "section_title": "React Basics",
+          "contents": [
+            "What is React? (10:00)",
+            "Introduction to JSX (08:30)"
+          ]
+        }
+      ],
+      "articles": 40,
+      "downloadable_resources": 20,
+      "on_demand_video": "40h",
+      "coding_exercises": 10,
+      "certificate_of_completion": true
+    },
+    {
+      "id": 5,
+      "title": "Node.js, Express, MongoDB & More: The Complete Bootcamp",
+      "description": "Learn to build modern, scalable web applications using Node.js, Express, and MongoDB.",
+      "image_url": "https://image.url/nodejs-course.jpg",
+      "author": "Jonas Schmedtmann",
+      "students": 800000,
+      "total_lectures": 254,
+      "total_hours": "30h 30m",
+      "price": 149.99,
+      "discounted_price": 11.99,
+      "discount_percentage": 92,
+      "course_contents": [
+        {
+          "section_title": "Node.js Fundamentals",
+          "contents": [
+            "Introduction to Node.js (12:00)",
+            "Setting up Node.js environment (10:30)"
+          ]
+        }
+      ],
+      "articles": 35,
+      "downloadable_resources": 15,
+      "on_demand_video": "30h",
+      "coding_exercises": 8,
+      "certificate_of_completion": true
+    },
+    {
+      "id": 6,
+      "title": "Angular - The Complete Guide",
+      "description": "Master Angular and build amazing, reactive web apps with the successor of Angular.js.",
+      "image_url": "https://image.url/angular-course.jpg",
+      "author": "Maximilian Schwarzmüller",
+      "students": 970000,
+      "total_lectures": 490,
+      "total_hours": "58h 33m",
+      "price": 199.99,
+      "discounted_price": 17.99,
+      "discount_percentage": 91,
+      "course_contents": [
+        {
+          "section_title": "Introduction to Angular",
+          "contents": [
+            "What is Angular? (10:00)",
+            "Building your first Angular app (09:15)"
+          ]
+        }
+      ],
+      "articles": 50,
+      "downloadable_resources": 28,
+      "on_demand_video": "58h",
+      "coding_exercises": 15,
+      "certificate_of_completion": true
+    },
+    {
+      "id": 7,
+      "title": "Complete Ethical Hacking Bootcamp 2024",
+      "description": "Learn ethical hacking from scratch and become a cybersecurity expert.",
+      "image_url": "https://image.url/ethical-hacking.jpg",
+      "author": "Zaid Sabih",
+      "students": 1100000,
+      "total_lectures": 340,
+      "total_hours": "45h 20m",
+      "price": 249.99,
+      "discounted_price": 13.99,
+      "discount_percentage": 94,
+      "course_contents": [
+        {
+          "section_title": "Hacking Basics",
+          "contents": [
+            "Setting up your hacking lab (08:30)",
+            "Networking fundamentals for hackers (10:45)"
+          ]
+        }
+      ],
+      "articles": 55,
+      "downloadable_resources": 30,
+      "on_demand_video": "45h",
+      "coding_exercises": 14,
+      "certificate_of_completion": true
+    },
+    {
+      "id": 8,
+      "title": "Digital Marketing Masterclass",
+      "description": "Learn everything you need to know to become a digital marketing expert in this masterclass.",
+      "image_url": "https://image.url/digital-marketing.jpg",
+      "author": "Phil Ebiner",
+      "students": 700000,
+      "total_lectures": 320,
+      "total_hours": "50h 10m",
+      "price": 199.99,
+      "discounted_price": 15.99,
+      "discount_percentage": 92,
+      "course_contents": [
+        {
+          "section_title": "Introduction to Digital Marketing",
+          "contents": [
+            "What is digital marketing? (09:00)",
+            "Creating a digital marketing strategy (10:00)"
+          ]
+        }
+      ],
+      "articles": 45,
+      "downloadable_resources": 25,
+      "on_demand_video": "50h",
+      "coding_exercises": 9,
+      "certificate_of_completion": true
+    },
+    {
+      "id": 9,
+      "title": "Photography Masterclass: A Complete Guide to Photography",
+      "description": "Learn the art of photography and start taking professional photos with this comprehensive guide.",
+      "image_url": "https://image.url/photography.jpg",
+      "author": "Phil Ebiner",
+      "students": 200000,
+      "total_lectures": 400,
+      "total_hours": "60h 20m",
+      "price": 149.99,
+      "discounted_price": 11.99,
+      "discount_percentage": 92,
+      "course_contents": [
+        {
+          "section_title": "Photography Basics",
+          "contents": [
+            "Understanding your camera (12:00)",
+            "Photography composition techniques (15:30)"
+          ]
+        }
+      ],
+      "articles": 60,
+      "downloadable_resources": 40,
+      "on_demand_video": "60h",
+      "coding_exercises": 5,
+      "certificate_of_completion": true
+    },
+    {
+      "id": 10,
+      "title": "Web Development Bootcamp",
+      "description": "Learn front-end and back-end web development with this comprehensive bootcamp.",
+      "image_url": "https://image.url/web-development.jpg",
+      "author": "Colt Steele",
+      "students": 1500000,
+      "total_lectures": 500,
+      "total_hours": "70h 35m",
+      "price": 199.00,
+      "discounted_price": 14.99,
+      "discount_percentage": 93,
+      "course_contents": [
+        {
+          "section_title": "HTML, CSS, and JavaScript Basics",
+          "contents": [
+            "Introduction to HTML (08:00)",
+            "Building your first webpage (12:15)"
+          ]
+        }
+      ],
+      "articles": 50,
+      "downloadable_resources": 35,
+      "on_demand_video": "70h",
+      "coding_exercises": 20,
+      "certificate_of_completion": true
+    },
+    {
+      "id": 11,
+      "title": "Data Structures and Algorithms: Deep Dive Using Java",
+      "description": "Learn data structures and algorithms with hands-on Java implementation.",
+      "image_url": "https://image.url/data-structures.jpg",
+      "author": "Tim Buchalka",
+      "students": 900000,
+      "total_lectures": 410,
+      "total_hours": "50h 10m",
+      "price": 149.99,
+      "discounted_price": 12.99,
+      "discount_percentage": 91,
+      "course_contents": [
+        {
+          "section_title": "Introduction to Data Structures",
+          "contents": [
+            "Understanding arrays (15:00)",
+            "Linked Lists (13:30)"
+          ]
+        }
+      ],
+      "articles": 55,
+      "downloadable_resources": 20,
+      "on_demand_video": "50h",
+      "coding_exercises": 18,
+      "certificate_of_completion": true
+    },
+    {
+      "id": 12,
+      "title": "iOS App Development for Beginners",
+      "description": "Learn how to build iOS apps from scratch with Swift and Xcode.",
+      "image_url": "https://image.url/ios-development.jpg",
+      "author": "Angela Yu",
+      "students": 850000,
+      "total_lectures": 350,
+      "total_hours": "45h 30m",
+      "price": 199.99,
+      "discounted_price": 16.99,
+      "discount_percentage": 91,
+      "course_contents": [
+        {
+          "section_title": "iOS Development Basics",
+          "contents": [
+            "Introduction to Swift (12:00)",
+            "Building your first iOS app (15:00)"
+          ]
+        }
+      ],
+      "articles": 35,
+      "downloadable_resources": 25,
+      "on_demand_video": "45h",
+      "coding_exercises": 14,
+      "certificate_of_completion": true
+    },
+    {
+      "id": 13,
+      "title": "Unity Game Development: Ultimate Guide",
+      "description": "Learn to create stunning 2D and 3D games with Unity and C#.",
+      "image_url": "https://image.url/unity-game-development.jpg",
+      "author": "Ben Tristem",
+      "students": 700000,
+      "total_lectures": 300,
+      "total_hours": "40h 40m",
+      "price": 149.99,
+      "discounted_price": 10.99,
+      "discount_percentage": 93,
+      "course_contents": [
+        {
+          "section_title": "Unity Basics",
+          "contents": [
+            "Introduction to Unity (10:00)",
+            "Creating your first game (14:30)"
+          ]
+        }
+      ],
+      "articles": 30,
+      "downloadable_resources": 15,
+      "on_demand_video": "40h",
+      "coding_exercises": 8,
+      "certificate_of_completion": true
+    },
+    {
+      "id": 14,
+      "title": "Cyber Security Training for Beginners",
+      "description": "Learn the basics of cybersecurity and protect yourself and your organization from attacks.",
+      "image_url": "https://image.url/cyber-security.jpg",
+      "author": "Nathan House",
+      "students": 900000,
+      "total_lectures": 220,
+      "total_hours": "32h 25m",
+      "price": 199.00,
+      "discounted_price": 18.99,
+      "discount_percentage": 90,
+      "course_contents": [
+        {
+          "section_title": "Cybersecurity Essentials",
+          "contents": [
+            "Understanding cybersecurity threats (10:45)",
+            "Setting up a secure system (12:30)"
+          ]
+        }
+      ],
+      "articles": 20,
+      "downloadable_resources": 10,
+      "on_demand_video": "32h",
+      "coding_exercises": 6,
+      "certificate_of_completion": true
+    },
+    {
+      "id": 15,
+      "title": "Blockchain and Cryptocurrency Explained",
+      "description": "Get a comprehensive introduction to blockchain technology and cryptocurrencies.",
+      "image_url": "https://image.url/blockchain.jpg",
+      "author": "George Levy",
+      "students": 400000,
+      "total_lectures": 180,
+      "total_hours": "28h 15m",
+      "price": 149.00,
+      "discounted_price": 13.99,
+      "discount_percentage": 90,
+      "course_contents": [
+        {
+          "section_title": "Introduction to Blockchain",
+          "contents": [
+            "What is Blockchain? (10:15)",
+            "Understanding cryptocurrency (08:45)"
+          ]
+        }
+      ],
+      "articles": 15,
+      "downloadable_resources": 10,
+      "on_demand_video": "28h",
+      "coding_exercises": 4,
+      "certificate_of_completion": true
+    }
+  ]
+};
+  
+exports.courses = courses.courses;
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+const courses = require('./response.js')
+
+//lets us send/receive JSON response
+app.use(bodyParser.json());
+
+//sets up cors request
+const cors = require('cors');
+app.use(cors());
+
+//initates and sets our port
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+});
+
+
+
+app.get('/courses', async (req, res) => {
+  //the star here is a wildcard, it means we are accepting request from any IP. This is typically unsafe to use in production.
+  res.header("Access-Control-Allow-Origin", "*");
+  const query = req.body;
+  const response = await handleGetCourses(query.id);
+  res.status(200).send(response)
+  });
+
+function handleGetCourses (query){
+    let response;
+    try {
+      for (const course of courses.courses){
+       if (query == course.id) response = course;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    return response;
+
+}
